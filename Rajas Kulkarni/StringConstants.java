06d46c4477c255b90c5034320c217850aeb527a3
Rajas Ajay Kulkarni <47221329+kulkarni-rajas@users.noreply.github.com>
2020-10-03 12:22:40 +0530

    [ADD] Codes

---
+++ Rajas Kulkarni/StringConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rms;
+
+/**
+ *
+ * @author Rajas
+ */
+public class StringConstants {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/rms"; //Put your mysql database URL here.
+    public static final String USER = "root"; // Put your mysql username here.
+    public static final String PASS = "password"; // Put your mysql password here.
+}
