f0b1b772f972c3faf0a91b9a0e47c0c7476f82ef
rishi2156 <65584715+rishi2156@users.noreply.github.com>
2020-10-10 22:52:14 +0530

    [ADD]Files for Frontend.

---
+++ RMS/src/rms/StringConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rms;
+
+/**
+ *
+ * @author rammy
+ */
+public class StringConstants {
+    public static final String DB_URL="jdbc:mysql://localhost:3306/testdb";
+    public static final String USER="root";
+    public static final String PASS="methane2156"; //Enter your root password here
+}
